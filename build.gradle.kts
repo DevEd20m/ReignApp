9d24141120892279acc7269754b0eb88948dc5c6
Edmundo <deved20m@gmail.com>
2021-11-20 02:53:18 -0500

    fixed build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.language.nativeplatform.internal.Dimensions.applicationVariants
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -6,8 +8,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-        classpath("com.android.tools.build:gradle:${Versions.gradle}")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}")
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
