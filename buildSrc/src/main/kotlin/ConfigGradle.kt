36f5f8651d179d8917df5e4a8b6da81579939a33
Edmundo <deved20m@gmail.com>
2021-11-25 09:50:47 -0500

    fixed problems with dependencies

---
+++ buildSrc/src/main/kotlin/ConfigGradle.kt
@@ -0,0 +1,14 @@
+object ConfigGradle {
+
+    object Releases {
+        const val versionCode = 1
+        const val versionName = "1.0"
+    }
+
+
+    object Module {
+        //Add your modules here
+        const val data = ":data"
+        const val domain = ":domain"
+    }
+}
